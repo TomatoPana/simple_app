c8703498eeba7d7df62444c06b695851c5e0f9ea
Moises David Lozano Bobadilla <mdlb.lobo@gmail.com>
2023-08-15 16:11:55 -0600

    wip: forms

--- app/src/main/java/com/mdlb/basicapplication/activities/MainActivity.kt
+++ app/src/main/java/com/mdlb/basicapplication/activities/MainActivity.kt
@@ -12,26 +12,26 @@ import com.mdlb.basicapplication.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var emptyLayout: ConstraintLayout
+//    private lateinit var recyclerView: RecyclerView
+//    private lateinit var emptyLayout: ConstraintLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        recyclerView = findViewById(R.id.list_layout)
-        emptyLayout = findViewById(R.id.empty_list_layout)
-
-        // TODO: Create DTO
-        val data = ArrayList<String>()
-
-        if (data.isEmpty()) {
-            emptyLayout.visibility = View.VISIBLE
-            recyclerView.visibility = View.GONE
-        } else {
-            emptyLayout.visibility = View.GONE
-            recyclerView.visibility = View.VISIBLE
-        }
+//        recyclerView = findViewById(R.id.list_layout)
+//        emptyLayout = findViewById(R.id.empty_list_layout)
+//
+//        // TODO: Create DTO
+//        val data = ArrayList<String>()
+//
+//        if (data.isEmpty()) {
+//            emptyLayout.visibility = View.VISIBLE
+//            recyclerView.visibility = View.GONE
+//        } else {
+//            emptyLayout.visibility = View.GONE
+//            recyclerView.visibility = View.VISIBLE
+//        }
 
         binding.floatingActionButton.setOnClickListener {
             startActivity(Intent(this, EditorActivity::class.java))
