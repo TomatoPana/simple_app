8e01342ca6d619fedc827724f69902631b7d50cc
Moises David Lozano Bobadilla <mdlb.lobo@gmail.com>
2023-08-15 18:05:59 -0600

    wip: Edit activity

--- app/src/main/java/com/mdlb/basicapplication/activities/EditorActivity.kt
+++ app/src/main/java/com/mdlb/basicapplication/activities/EditorActivity.kt
@@ -4,22 +4,26 @@ import android.os.Bundle
 import android.view.MotionEvent
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
+import com.mdlb.basicapplication.R
 import com.mdlb.basicapplication.databinding.ActivityEditorBinding
 
 class EditorActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditorBinding
     private lateinit var calledBy: String
+    private lateinit var firstNameEditBox: TextInputLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        calledBy = intent.getStringExtra("calledBy") ?: ""
         binding = ActivityEditorBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        calledBy = intent.getStringExtra("calledBy") ?: ""
+        firstNameEditBox = findViewById(R.id.firstNameTextInputLayout);
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        Toast.makeText(this, this.calledBy, Toast.LENGTH_LONG).show()
+        Toast.makeText(this, this.calledBy + firstNameEditBox.editText?.text.toString(), Toast.LENGTH_LONG).show()
         return super.onTouchEvent(event)
     }
 }
